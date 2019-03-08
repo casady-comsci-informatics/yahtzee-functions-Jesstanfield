b06502c6bd54e199c9308beee32c5cbeb561cdd0
Jes Stanfield <jesstanfield@casady.org>
2019-03-07 21:27:33 -0600

    done

--- yahtzee.R
+++ yahtzee.R
@@ -1,15 +1,20 @@
-roll <- function(die, size, replace){
-
+roll <- function(die = 1:6, size = 2, replace = T){
+  sample(die, size, replace)
 }
 
 yahtzeeRoll <- function(){
-
+  replicate(5, roll(1:6, 1))
 }
 
 sumRoll <- function(roll, value){
-
+  for (i in 1:5) {
+    if (roll[i] != value) {
+      return(FALSE)
+    }
+  }
+  return(TRUE)
 }
 
-isYahtzee(roll){
-
+isYahtzee <- function(roll){
+  sumRoll(roll, roll[1])
 }
